5ff9d3414d08dfd7e1e4341eef99bd73b6f36a4c
David Gloyn-Cox <dgloyncox@rockymountaineer.com>
2019-03-06 12:13:46 -0800

    Init framework

--- kaptiorestpython/client.py
+++ kaptiorestpython/client.py
@@ -2,3 +2,13 @@ import time
 from datetime import datetime
 from kaptiorestpython.helper.http_lib import HttpLib
 from kaptiorestpython.helper.exceptions import APIException
+
+def has_empty_warning(result):
+    if 'result' not in result \
+        and 'warnings' in result \
+        and len(result['warnings']) \
+        and result['warnings'][0] == 'No assets found for the given search criteria.':
+        return True
+
+    return False
+

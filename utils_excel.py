11a352a6ccbd187bf3e63cdbae60629013d47f35
David Gloyn-Cox <dgloyncox@rockymountaineer.com>
2019-05-28 16:16:27 -0700

    added equation to excel tool

--- utils_excel.py
+++ utils_excel.py
@@ -7,9 +7,16 @@ from datetime import datetime, timedelta
 from openpyxl import load_workbook
 from openpyxl.utils import column_index_from_string
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, Color
+from Equation import Expression
 import logging
 
 logger = logging.getLogger(__name__)
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
 
 def load_WB(path):    
     # process each sheet...
@@ -91,16 +98,28 @@ def load_bulkloaderconfig(config_path):
     logger.info("Loaded bulkloader config data...")
     return config
 
+def get_field(field, row):
+    key = field['field']
+    if key in row:
+        if 'format' in field and 'type' in field:
+            field_type = field['type']
+            if field_type == 'date':
+                return row[key].strftime(field['format'])
+            else:
+                return row[key]
+        else:    
+            return row[key]
+
 def generate_bulkloader(price_data, savepath, template, yearnumber, versionnumber, tax_profile, config, currency="CAD"):
     excel_feed_path = os.path.join(savepath, 'templates', template)
-    bulk_file_name = 'Rocky Bulk Loader.{}.{}.{}.{}.xlsx'.format(tax_profile.replace(" ", ""), yearnumber, versionnumber, currency)
+    bulk_file_name = 'RM Bulk Loader.{}.{}.{}.{}.xlsx'.format(tax_profile.replace(" ", ""), yearnumber, versionnumber, currency)
 
     row_idx = config['start_row']-1
 
     logger.info('=== Styles ===')
     style_fills = {}
     if 'fills' in config:
-        logger.info('\t=== Files ===')
+        logger.info('\t=== Fills ===')
         for key in config['fills']:
             logger.info('\t\t{} -> {}'.format(key, config['fills'][key]))
             style_fills[key] = PatternFill(**config['fills'][key])
@@ -116,7 +135,7 @@ def generate_bulkloader(price_data, savepath, template, yearnumber, versionnumbe
 
     style_sides = {}
     if 'sides' in config:
-        logger.info('\t=== Sides ===')
+        logger.info('\t=== Borders ===')
         for key in config['sides']:
             logger.info('\t\t{} -> {}'.format(key, config['sides'][key]))
             style_sides[key] = Side(**config['sides'][key])
@@ -124,67 +143,72 @@ def generate_bulkloader(price_data, savepath, template, yearnumber, versionnumbe
 
     wb = load_WB(excel_feed_path)
     ws = wb[config['sheetname']]
-    ws.title = "{}".format(yearnumber)
-
+    for s in config.get('sheets'):
+        ws_new = wb.copy_worksheet(ws)
+        ws_new.title = "{}".format(s.get('name'))
+    wb.remove_sheet(ws)
+    if 'Sheet1' in wb.sheetnames:
+        ws = wb['Sheet1']
+        wb.remove_sheet(ws)
+    
+    short = 30
     for row in price_data:
         row_idx += 1 
-        for field in config['fieldmap']:
-            coord = '{}{}'.format(field['column'], row_idx)
-        
-            # format the field..
-            # format the field..
-            key_name = 'fill'
-            if key_name in field:
-                ws[coord].fill = style_fills[field[key_name]]
-                
-            key_name = 'font'
-            if key_name in field:
-                ws[coord].font = style_fonts[field[key_name]]
+        row['comm'] = 0.2
 
-            key_name = 'border'
-            if key_name in field:
-                cSide = style_sides[field[key_name]]
-                ws[coord].border = Border(left=cSide , right=cSide, top=cSide, bottom=cSide)
-                
-            # condition is a check for NONE in the specified field is none skip that record...
-            clear_cell = False
-            if 'condition' in field:
-                condition_field = field['condition']
-                
-                if condition_field in row:
-                    if row[condition_field] is None or row[condition_field] == '':
+        for s in config.get('sheets',{}):
+            ws = wb[s.get('name')]
+
+            for field in s.get('fieldmap',[]):
+                coord = '{}{}'.format(field['column'], row_idx)
+                # format the field..
+                key_name = 'fill'
+                if key_name in field:
+                    ws[coord].fill = style_fills[field[key_name]]
+                    
+                key_name = 'font'
+                if key_name in field:
+                    ws[coord].font = style_fonts[field[key_name]]
+
+                key_name = 'border'
+                if key_name in field:
+                    cSide = style_sides[field[key_name]]
+                    ws[coord].border = Border(left=cSide , right=cSide, top=cSide, bottom=cSide)
+                    
+                # condition is a check for NONE in the specified field is none skip that record...
+                clear_cell = False
+                if 'condition' in field:
+                    condition_field = field['condition']
+                    
+                    if condition_field in row:
+                        if row[condition_field] is None or row[condition_field] == '':
+                            clear_cell = True
+                    else:
                         clear_cell = True
-                else:
-                    clear_cell = True
 
-            if clear_cell:
-                ws[coord].value = None
-                continue
-                
-            if 'field' in field:
-                key = field['field']
-                if key in row:
-                    if 'format' in field and 'type' in field:
-                        field_type = field['type']
-                        if field_type == 'date':
-                            ws[coord].value = row[key].strftime(field['format'])
-                        else:
-                            ws[coord].value = row[key]
-                    else:    
-                        ws[coord].value = row[key]
-                        
-            elif 'formula' in field:
-                formula = field['formula']
-                
-                formula = formula.replace('{r}', str(row_idx))
-                ws[coord].value = formula
-            
-            elif 'equation' in field:
-                pass
-                
-            elif 'value' in field:
-                ws[coord].value = field['value']
-                        
+                if clear_cell:
+                    ws[coord].value = None
+                    continue
+                    
+                if 'field' in field:
+                    ws[coord].value = get_field(field, row)
+
+                elif 'equation' in field:
+                    # split on spaces...
+                    fn = Expression(field.get('equation'))
+                    t = {}
+                    for f in fn:
+                        t[f] = num(row.get(f,0))
+                    ws[coord].value = fn(**t)
+                            
+                elif 'formula' in field:
+                    formula = field['formula']
+                    formula = formula.replace('{r}', str(row_idx))
+                    ws[coord].value = formula
+                                
+                elif 'value' in field:
+                    ws[coord].value = field['value']
+
     excel_save_path = os.path.join(savepath, 'data', bulk_file_name)
     wb.save(excel_save_path)
 
